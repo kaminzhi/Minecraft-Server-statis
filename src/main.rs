e35b898990c43860d6b8cfd0a17bf06560df60d0
kaminzhi <issac20050710@gmail.com>
2024-12-30 14:26:01 +0800

    fix: fix favicon response

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use dotenv::dotenv;
 use minecraft::{parse_response, read_server_response};
 use protocol::{send_handshake, send_status_request};
 use std::env;
+use std::io::{self, Write};
 use tokio::net::TcpStream;
 
 #[tokio::main]
@@ -19,31 +20,55 @@ async fn main() -> tokio::io::Result<()> {
         .parse()
         .expect("PORT must be a valid u16");
 
-    let mut stream = TcpStream::connect(&server_address)
-        .await
-        .expect("Unable to connect to the server");
+    loop {
+        print!("Enter command (exit to quit):");
+        io::stdout().flush()?;
 
-    send_handshake(&mut stream, &host, port).await?;
-    send_status_request(&mut stream).await?;
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+        let input = input.trim();
 
-    let response = read_server_response(&mut stream).await?;
-    match parse_response(&response) {
-        Ok(data) => {
-            println!("Description: {}", data.description);
-            println!("Players: {}/{}", data.players.online, data.players.max);
-            println!(
-                "Version: {} (protocol {})",
-                data.version.name, data.version.protocol
-            );
+        if input == "exit" {
+            break;
+        } else if input == "help" {
+            println!("  get status  - Fetch server status");
+            println!("  exit        - Exit the program");
+        } else if input == "get status" {
+            let mut stream = TcpStream::connect(&server_address)
+                .await
+                .expect("Unable to connect to the server");
 
-            if let Some(favicon_data) = data.favicon {
-                println!("Favicon base64 data: {}", favicon_data);
+            send_handshake(&mut stream, &host, port).await?;
+            send_status_request(&mut stream).await?;
+
+            let response = read_server_response(&mut stream).await?;
+            match parse_response(&response) {
+                Ok(data) => {
+                    println!("Description: {}", data.description);
+                    println!("Players: {}/{}", data.players.online, data.players.max);
+                    println!(
+                        "Version: {} (protocol {})",
+                        data.version.name, data.version.protocol
+                    );
+                    if let Some(favicon_data) = data.favicon {
+                        if let Some(index) = favicon_data.find(",") {
+                            let actual_base64 = &favicon_data[index + 1..];
+                            println!("Favicon base64 data: {}", actual_base64);
+                        } else {
+                            println!("Favicon base64 data: {}", favicon_data);
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!("Error parsing server response: {}", e);
+                }
             }
-        }
-        Err(e) => {
-            println!("Error parsing server response: {}", e);
+        } else {
+            println!("Unknown command: {}", input);
         }
     }
+
+    println!("Exiting...");
 
     Ok(())
 }
